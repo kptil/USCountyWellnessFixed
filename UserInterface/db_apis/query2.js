c10ddb3c4540edeb4713998d019d672973e33d9a
kptil <ktiley@ufl.edu>
2022-11-28 07:46:54 -0500

    graph built in fetch

--- UserInterface/db_apis/query2.js
+++ UserInterface/db_apis/query2.js
@@ -1,17 +1,16 @@
 const db = require('../services/database.js');
 const tb = require('../table.js')
 
-const baseQuery = `SELECT state_name, county_em_year, num / denom as rate
+/*const baseQuery = `SELECT state_name, county_em_year, num / denom as rate
                     from (
                         select state_name, county_em_year, sum(labor_force) as denom, sum(unemployed) as num
                         from (
                             (select coid, state_name from ${tb.tables.countyTest} where state_name = 'Florida')
                             natural join ${tb.tables.countyHasEmploymentTest}
                         )
-                    where state_name = 'Florida'
                     group by state_name, county_em_year
                     )
-                    where (county_em_year >= 2000 and county_em_year <= 2005)`;
+                    where (county_em_year >= 2000 and county_em_year <= 2005)`;*/
 
 async function find(context) {
     let query = `SELECT state_name, county_em_year, num / denom as rate
